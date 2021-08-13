3f7e680132be52799bcfdef5a4f753d03d2e0dfc
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-08-12 22:58:38 -0400

    correct add employee; update emp role

--- index.js
+++ index.js
@@ -36,6 +36,23 @@ const menuQuestions = [
 /* END VARIABLES */
 
 /* FUNCTIONS */
+//Map employee Array from SQL for use in Inquire list array to select employee to delete
+const fEmp = function (item) {
+  var newArr = {};
+  newArr.name = item.name;
+  newArr.value = item.id;
+  return newArr;
+};
+
+//Map Role Array from SQL for use in Inquire list array
+const fRole = function (item) {
+  console.log(item);
+  var newArr = {};
+  newArr.name = item.title;
+  newArr.value = item.id;
+  return newArr;
+};
+
 function viewEmployees(orderBy) {
   const sql = `SELECT 
     e.id as ID, 
@@ -51,7 +68,7 @@ function viewEmployees(orderBy) {
   JOIN department ON role.department_id = department.id 
   LEFT JOIN employee m ON m.id = e.manager_id 
   ORDER BY ${orderBy}`;
-  
+
   db.query(sql, function (err, results) {
     if (err) {
       console.log(err);
@@ -71,64 +88,110 @@ function viewRoles() {
   });
 };
 
-const promptCreateEmp = (roleArr) => {
-  console.log(`
-=============
-Add Employee
-=============
-`);
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'first_name',
-      message: "What is the employee's first name?",
-      validate: first_nameInput => {
-        if (first_nameInput) {
-          return true;
-        } else {
-          console.log("Please enter the employee's first name");
-          return false;
-        }
-      },
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: "What is the employee's last name?",
-      validate: last_nameInput => {
-        if (last_nameInput) {
-          return true;
-        } else {
-          console.log("Please enter the employee's last name");
-          return false;
-        }
-      },
-    },
-    {
-      type: 'list',
-      name: 'empRole',
-      message: "What is the employee's role?",
-      choices: roleArr
-    }
-  ]);
-};
-
-const fRole = function (item) {
-  var newArr = {};
-  newArr.name = item.title;
-  newArr.value = item.id;
-  return newArr;
-};
-
+//Create new employee
 const createEmp = () => {
+  //New Employee Prompt for data
+  const promptCreateEmp = (roleArr, empArr) => {
+    console.log(`
+  =============
+  Add Employee
+  =============
+  `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: "What is the employee's first name?",
+        validate: first_nameInput => {
+          if (first_nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the employee's first name");
+            return false;
+          }
+        },
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: "What is the employee's last name?",
+        validate: last_nameInput => {
+          if (last_nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the employee's last name");
+            return false;
+          }
+        },
+      },
+      {
+        type: 'list',
+        name: 'empRole',
+        message: "What is the employee's role?",
+        choices: roleArr
+      },
+      {
+        type: 'list',
+        name: 'empMrg',
+        message: "Who is the employee's manager?",
+        choices: empArr
+      }
+    ]);
+  };
+
   db.promise().query(`SELECT id, title FROM role`).then((results) => {
+    //Pull Role Index from SQL using Array.Map
     let roleArr = results[0].map(fRole);
-    console.log(roleArr);
-    promptCreateEmp(roleArr).then(empAnswers => {
-      const sql = `INSERT INTO employee 
-        (first_name, last_name, role_id) 
+    db.promise().query(`SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee`).then((results) => {
+      let empArr = results[0].map(fEmp);
+
+      //Prompt User for New Employee Data, Insert into SQL
+      promptCreateEmp(roleArr, empArr).then(empAnswers => {
+        const sql = `INSERT INTO employee 
+        (first_name, last_name, role_id, manager_id) 
       VALUES 
-        ("${empAnswers.first_name}","${empAnswers.last_name}",${empAnswers.empRole})`;
+        ("${empAnswers.first_name}","${empAnswers.last_name}",${empAnswers.empRole},${empAnswers.empMrg})`;
+
+        db.query(sql, function (err, results) {
+          if (err) {
+            console.log(err);
+          }
+          console.table(results);
+          menuLoop();
+        });
+      })
+    })
+  })
+};
+
+//Remove employee from SQL
+const deleteEmp = () => {
+  //List employees to select one to delete
+  const promptDeleteEmp = (empArr) => {
+    console.log(`
+  =========================
+  Select Employee To Remove
+  =========================
+  `);
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'empList',
+        message: "Which employee do you want to remove?",
+        choices: empArr
+      }
+    ]);
+  };
+
+  const sql = `SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee`;
+  db.promise().query(sql).then((results) => {
+    //Pull Role Index from SQL using Array.Map
+    let empArr = results[0].map(fEmp);
+
+    //Prompt User for New Employee Data, Insert into SQL
+    promptDeleteEmp(empArr).then(empAnswers => {
+      console.log(empAnswers)
+      const sql = `DELETE FROM employee WHERE id = ${empAnswers.empList}`;
 
       db.query(sql, function (err, results) {
         if (err) {
@@ -137,6 +200,59 @@ const createEmp = () => {
         console.table(results);
         menuLoop();
       });
+    })
+  })
+};
+
+//Update employee Role
+const updateEmpRole = () => {
+  //New Employee Prompt for data
+  const promptEmpRole = (roleArr, empArr) => {
+    console.log(`
+  =======================
+  To change Employee Role
+  =======================
+  `);
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'empList',
+        message: "Which employee do you want to change role?",
+        choices: empArr
+      },
+      {
+        type: 'list',
+        name: 'empRole',
+        message: "What is the employee's role?",
+        choices: roleArr
+      }
+    ]);
+  };
+
+  const sql = `SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee`;
+  db.promise().query(sql).then((results) => {
+    //Pull Role Index from SQL using Array.Map
+    let empArr = results[0].map(fEmp);
+
+    db.promise().query(`SELECT id, title FROM role`).then((results) => {
+      //Pull Role Index from SQL using Array.Map
+      let roleArr = results[0].map(fRole);
+
+      //Prompt User for New Employee Data, Insert into SQL
+      promptEmpRole(roleArr, empArr).then(empAnswers => {
+        console.log(empAnswers)
+        const sql = `UPDATE employee 
+            SET role_id = ${empAnswers.empRole}
+            WHERE id = ${empAnswers.empList}`;
+
+        db.query(sql, function (err, results) {
+          if (err) {
+            console.log(err);
+          }
+          console.table(results);
+          menuLoop();
+        });
+      })
     })
   })
 };
@@ -158,13 +274,12 @@ const menuLoop = function () {
           break;
         case "create":
           createEmp();
-
           break;
         case "delete":
-
+          deleteEmp();
           break;
         case "updateEmpRole":
-
+          updateEmpRole();
           break;
         case "updateEmpMrg":
 
